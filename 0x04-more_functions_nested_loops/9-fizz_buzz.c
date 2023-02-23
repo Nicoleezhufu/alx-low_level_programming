fe9bd242a871b17bd043b9e2593d481407437b48
nicoleezhufu <nicolecece44@gmail.com>
2023-02-23 15:46:53 -0800

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 {
 	int x;
 
-	for (x = i; x<= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-	if ((x % 3 ==0) && (x % 5 ==0))
+	if ((x % 3 == 0) && (x % 5 == 0))
 	{
 	printf("FizzBuzz");
 	}
 	else if (x % 3 == 0)
 	{
-	printf("Fizz"};
+	printf("Fizz");
 	}
 	else if (x % 5 == 0)
 	{
@@ -35,5 +35,5 @@ int main(void)
 	}
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
